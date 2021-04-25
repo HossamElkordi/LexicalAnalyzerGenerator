bf6775ecf8866c8ecb415c866f92a68df50e693f
Hossam Elkordi <hossam_elkordi@yahoo.com>
2021-04-25 13:56:03 +0200

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/LexicalAnalyzerGenerator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LexicalAnalyzerGenerator.dir/main.cpp.o"
+  "LexicalAnalyzerGenerator"
+  "LexicalAnalyzerGenerator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LexicalAnalyzerGenerator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
