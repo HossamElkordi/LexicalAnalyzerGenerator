9d8fff6b0289c7f0e356e3d1e1f8608082956736
Mina Ashraf <minachraf@outlook.com>
2021-05-05 15:24:19 +0200

    Created Token class

--- Token.cpp
+++ Token.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "Token.h"
+
+using namespace std;
+
+string Token::GetType() {
+    return this->type;
+}
+
+string Token::GetLexeme() {
+    return this->lexeme;
+}
+
+Token::Token(string type, string lexeme) {
+    this->lexeme=lexeme;
+    this->type=type;
+}
