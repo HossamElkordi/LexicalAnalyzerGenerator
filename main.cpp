a9550c35545c6af0810c4b96e4d0a4ad7b58efd6
Hossam Elkordi <hossam_elkordi@yahoo.com>
2021-05-08 18:28:36 +0200

    Fix Output

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char** argv){
     list<string> temp;
     gramPath = (argc > 2) ? argv[2] : "grammar.txt";
     inPath = (argc > 3) ? argv[3] : "input.txt";
-    inPath = (argc > 4) ? argv[4] : "result.txt";
+    outPath = (argc > 4) ? argv[4] : "result.txt";
 
     InputParser ip(gramPath);
     ip.readFile();
@@ -37,28 +37,6 @@ int main(int argc, char** argv){
 
     Nfa nfa = Nfa();
     nfa = nfa.getfromlist(regs);
-//_____________________
-//    for(map<int,map<string,vector<int>>>::iterator it = nfa.transitions.begin(); it != nfa.transitions.end(); ++it) {
-//        cout<<it->first<<"--> ";
-//        for(map<string,vector<int>>::iterator it2 = it->second.begin(); it2 != it->second.end(); ++it2) {
-//            cout<<"   "<<it2->first;
-//            for (int j = 0; j < it2->second.size(); ++j) {
-//                cout<<"-"<<it2->second[j]<<"  ";
-//            }
-//            cout<<endl;
-//        }
-//        cout<<endl;
-//    }
-//    for (int i = 0; i < nfa.accepting.size(); ++i) {
-//        cout<<"acc "<<nfa.accepting[i]<<endl;
-//    }
-//    for(map<int,string>::iterator it2 = nfa.tags.begin(); it2 != nfa.tags.end(); ++it2) {
-//        cout<<it2->first<<" "<<it2->second<<endl;
-//    }
-//    cout<<nfa.end<<endl<<endl;
-    //_____________________________
-
-
 
     Dfa dfa(nfa.transitions, nfa.getAlphabets(), nfa.tags, ip.getRegexPriority());
     dfa.createDFA();
@@ -75,7 +53,6 @@ int main(int argc, char** argv){
 
     InputLanguageParser ilp(dfaGraph, dfaAccepted, start);
     vector<Token> toks = ilp.parseFile(inPath);
-    cout<<toks.empty();
 
     OutputGenerator op;
     op.outputTable(dfaForm);
